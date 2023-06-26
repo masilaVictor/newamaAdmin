9e622a39c86fad0d5d03bb3d51bd31b533e2b838
Victor Masila <masila.victor43@gmail.com>
2023-06-26 19:14:03 +0300

    second

--- lib/auth/test.dart
+++ lib/auth/test.dart
@@ -10,10 +10,63 @@ class Test extends StatefulWidget {
 class _TestState extends State<Test> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Testing'),
-        backgroundColor: Colors.red,
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+            decoration: BoxDecoration(
+              color: Colors.blue
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Container(
+                    transform: Matrix4.translationValues(0, -60.0, 0),
+                    child: Image.asset('assets/images/newama2.png',color: Colors.red,),
+                  )
+
+                ),
+                
+                Container(
+                    transform: Matrix4.translationValues(0, -150, 0.0),
+                    margin: EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(topRight: Radius.circular(40),topLeft: Radius.circular(40))
+                    ),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 600,
+                      child: Column(
+                        children: [
+                          Image.asset('assets/images/rider.png'),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(70, 0, 70, 0),
+                            transform: Matrix4.translationValues(0, -60, 0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ElevatedButton(onPressed: (){}, 
+                                child: Text('Store Panel', style: TextStyle(fontSize: 17),),
+                                ),
+                                ElevatedButton(onPressed: (){}, 
+                                child: Text('Admin',style: TextStyle(fontSize: 17)))
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  
+                )
+              ],
+              
+            ),
+
+          
+        ),
+        
       ),
     );
   }
